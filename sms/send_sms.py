ccc6168e013880f8e382ccaae0fbf3ad9a98b1a5
vin33t <vineet.chd09@gmail.com>
2018-12-07 18:06:25 +0530

    tweaks_v1_

--- sms/send_sms.py
+++ sms/send_sms.py
@@ -1,10 +1,8 @@
 import requests
 
-base_url = ["api.postcodes.io/postcodes/"]
-
 
 def send_sms(postal_code):
-    request_url = (base_url + '%s' + '?') % (postal_code)
+    request_url = ('api.postcodes.io/postcodes/' + postal_code)
     response = requests.get(request_url).json()
     if response['status'] == 200:
         return True
